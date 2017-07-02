4b77566f535a20ebe035c6a9b759cbcf401d708b
Chris Baker <chris@cbaker.co.uk>
2017-07-02 22:12:03 +0100

    Use LibGDX Bullet collision world example as a base for the proof of concept

---
+++ desktop/src/com/base2/bullet2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.base2.bullet2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.base2.bullet2d.Bullet2DTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bullet2DTest(), config);
+	}
+}
