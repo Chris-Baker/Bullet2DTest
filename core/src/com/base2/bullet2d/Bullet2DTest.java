1e1bbfa029b0457254c104966b84f9a68edd5d1c
Chris Baker <chris@cbaker.co.uk>
2017-07-04 00:00:02 +0100

    Start writing 2D debug draw

--- core/src/com/base2/bullet2d/Bullet2DTest.java
+++ core/src/com/base2/bullet2d/Bullet2DTest.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
@@ -53,12 +54,7 @@ public class Bullet2DTest extends ApplicationAdapter {
 
 	class MyContactListener extends ContactListener {
 		@Override
-		public boolean onContactAdded (int userValue0, int partId0, int index0, boolean match0, int userValue1, int partId1,
-									   int index1, boolean match1) {
-			if (match0)
-				((ColorAttribute)instances.get(userValue0).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
-			if (match1)
-				((ColorAttribute)instances.get(userValue1).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+		public boolean onContactAdded (int userValue0, int partId0, int index0, boolean match0, int userValue1, int partId1, int index1, boolean match1) {
 			return true;
 		}
 	}
@@ -77,13 +73,14 @@ public class Bullet2DTest extends ApplicationAdapter {
 		}
 	}
 
-	static class GameObject extends ModelInstance implements Disposable {
+	static class GameObject implements Disposable {
 		public final btRigidBody body;
 		public final MyMotionState motionState;
+		public final Matrix4 transform;
 
-		public GameObject (Model model, String node, btRigidBody.btRigidBodyConstructionInfo constructionInfo) {
-			super(model, node);
+		public GameObject (btRigidBody.btRigidBodyConstructionInfo constructionInfo) {
 			motionState = new MyMotionState();
+			transform = new Matrix4();
 			motionState.transform = transform;
 			body = new btRigidBody(constructionInfo);
 			body.setMotionState(motionState);
@@ -98,15 +95,11 @@ public class Bullet2DTest extends ApplicationAdapter {
 		}
 
 		static class Constructor implements Disposable {
-			public final Model model;
-			public final String node;
 			public final btCollisionShape shape;
 			public final btRigidBody.btRigidBodyConstructionInfo constructionInfo;
 			private static Vector3 localInertia = new Vector3();
 
-			public Constructor (Model model, String node, btCollisionShape shape, float mass) {
-				this.model = model;
-				this.node = node;
+			public Constructor (btCollisionShape shape, float mass) {
 				this.shape = shape;
 				if (mass > 0f)
 					shape.calculateLocalInertia(mass, localInertia);
@@ -116,7 +109,7 @@ public class Bullet2DTest extends ApplicationAdapter {
 			}
 
 			public GameObject construct () {
-				return new GameObject(model, node, constructionInfo);
+				return new GameObject(constructionInfo);
 			}
 
 			@Override
@@ -128,7 +121,6 @@ public class Bullet2DTest extends ApplicationAdapter {
 	}
 
 	OrthographicCamera cam;
-	CameraInputController camController;
 	ModelBatch modelBatch;
 	Environment environment;
 	Model model;
@@ -142,6 +134,8 @@ public class Bullet2DTest extends ApplicationAdapter {
 	btBroadphaseInterface broadphase;
 	btDynamicsWorld dynamicsWorld;
 	btConstraintSolver constraintSolver;
+
+	ShapeRenderer shapeRenderer;
 
 	@Override
 	public void create () {
@@ -158,38 +152,13 @@ public class Bullet2DTest extends ApplicationAdapter {
 		cam.far = 300f;
 		cam.update();
 
-		camController = new CameraInputController(cam);
-		Gdx.input.setInputProcessor(camController);
-
-		ModelBuilder mb = new ModelBuilder();
-		mb.begin();
-		mb.node().id = "ground";
-		mb.part("ground", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.RED)))
-				.box(5f, 1f, 5f);
-		mb.node().id = "sphere";
-		mb.part("sphere", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.GREEN)))
-				.sphere(1f, 1f, 1f, 10, 10);
-		mb.node().id = "box";
-		mb.part("box", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.BLUE)))
-				.box(1f, 1f, 1f);
-		mb.node().id = "cone";
-		mb.part("cone", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.YELLOW)))
-				.cone(1f, 2f, 1f, 10);
-		mb.node().id = "capsule";
-		mb.part("capsule", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.CYAN)))
-				.capsule(0.5f, 2f, 10);
-		mb.node().id = "cylinder";
-		mb.part("cylinder", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal,
-				new Material(ColorAttribute.createDiffuse(Color.MAGENTA))).cylinder(1f, 2f, 1f, 10);
-		model = mb.end();
-
 		constructors = new ArrayMap<String, GameObject.Constructor>(String.class, GameObject.Constructor.class);
-		constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f)), 0f));
-		constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f), 1f));
-		constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
-		constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f), 1f));
-		constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f), 1f));
-		constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)),
+		constructors.put("ground", new GameObject.Constructor(new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f)), 0f));
+		constructors.put("sphere", new GameObject.Constructor(new btSphereShape(0.5f), 1f));
+		constructors.put("box", new GameObject.Constructor(new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
+		constructors.put("cone", new GameObject.Constructor(new btConeShape(0.5f, 2f), 1f));
+		constructors.put("capsule", new GameObject.Constructor(new btCapsuleShape(.5f, 1f), 1f));
+		constructors.put("cylinder", new GameObject.Constructor(new btCylinderShape(new Vector3(.5f, 1f, .5f)),
 				1f));
 
 		collisionConfig = new btDefaultCollisionConfiguration();
@@ -197,7 +166,7 @@ public class Bullet2DTest extends ApplicationAdapter {
 		broadphase = new btDbvtBroadphase();
 		constraintSolver = new btSequentialImpulseConstraintSolver();
 		dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constraintSolver, collisionConfig);
-		dynamicsWorld.setGravity(new Vector3(0, -10f, 0));
+		dynamicsWorld.setGravity(new Vector3(0, -25f, 0));
 		contactListener = new MyContactListener();
 
 		instances = new Array<GameObject>();
@@ -208,6 +177,8 @@ public class Bullet2DTest extends ApplicationAdapter {
 		object.body.setContactCallbackFlag(GROUND_FLAG);
 		object.body.setContactCallbackFilter(0);
 		object.body.setActivationState(Collision.DISABLE_DEACTIVATION);
+
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	public void spawn () {
@@ -239,14 +210,17 @@ public class Bullet2DTest extends ApplicationAdapter {
 			spawnTimer = 1.5f;
 		}
 
-		camController.update();
-
 		Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1.f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		modelBatch.begin(cam);
-		modelBatch.render(instances, environment);
-		modelBatch.end();
+		shapeRenderer.setProjectionMatrix(cam.combined);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+		shapeRenderer.setColor(0, 1, 0, 1);
+		for (GameObject instance: instances) {
+			btCollisionShape shape = instance.body.getCollisionShape();
+			System.out.println(shape);
+		}
+		Gdx.app.exit();
 	}
 
 	@Override
